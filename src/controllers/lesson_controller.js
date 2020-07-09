0be4f3f65327fa6e4c86725f5f081fd6326010a6
soliakindrat99 <48919489+soliakindrat99@users.noreply.github.com>
2020-07-09 16:21:30 +0300

    add CREATE, UPDATE and DELETE lessons

--- src/controllers/lesson_controller.js
+++ src/controllers/lesson_controller.js
@@ -10,3 +10,51 @@ exports.get_lesson = (req, res) => {
         })
         .then((user) => res.json(user));
 };
+
+exports.create_lesson = (req, res) => {
+    lesson.create(req.body)
+        .then((lesson) => res.json(lesson))
+        .catch((err) => {
+            res.status(400).send({ message: err.message() || "Body can not be empty!!!" });
+        });
+};
+
+exports.delete_lesson = (req, res) => {
+    lesson.destroy({
+            where: { id: req.params.id }
+        })
+        .then((result) => {
+            if (result == 1) {
+                res.send({
+                    message: "The lesson was successfully deleted!"
+                });
+            } else {
+                res.send({
+                    message: "The lesson could not be deleted!"
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message || "Delete error" });
+        });
+};
+
+exports.update_lesson = (req, res) => {
+    lesson.update({
+            where: { id: req.params.id }
+        })
+        .then((result) => {
+            if (result == 1) {
+                res.send({
+                    message: "The lesson was successfully updated!"
+                });
+            } else {
+                res.send({
+                    message: "The lesson could not be updated!"
+                });
+            }
+        })
+        .catch((err) => {
+            res.status(500).send({ message: err.message || "Update error" });
+        });
+};
--- src/routes/lesson.js
+++ src/routes/lesson.js
@@ -4,6 +4,8 @@ const lesson_controller = require('../controllers/lesson_controller.js');
 router.get('/', lesson_controller.all_lessons);
 router.get('/:id', lesson_controller.get_lesson);
 
-
+router.post('/', lesson_controller.create_lesson);
+router.delete('/:id', lesson_controller.delete_lesson);
+router.put('/:id', lesson_controller.update_lesson);
 
 module.exports = router;
